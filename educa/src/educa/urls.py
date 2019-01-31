43004455d3e75a28e7141b89b4e1ea3305252b85
Shalva <sh.jashiashvili@gmail.com>
2019-01-31 13:44:37 +0400

    cms

--- educa/src/educa/urls.py
+++ educa/src/educa/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(
+        template_name='account/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(
+        template_name='account/logged_out.html'), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
 ]
